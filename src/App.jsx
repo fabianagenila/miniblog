fec0b02ef54063495b4a6cade72924c286d347a6
fabianagenila <fabianagenila@gmail.com>
2023-02-22 17:57:31 -0300

    firebase and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>APP</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbRy_6Jb-hAIgvTvfwW6uqaEcI0_abMAo",
+  authDomain: "mini-blog-ff4a5.firebaseapp.com",
+  projectId: "mini-blog-ff4a5",
+  storageBucket: "mini-blog-ff4a5.appspot.com",
+  messagingSenderId: "961759229681",
+  appId: "1:961759229681:web:7940dd5507ff5193a75b50",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
