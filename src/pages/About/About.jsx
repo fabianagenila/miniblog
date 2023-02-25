340c9efccdf850cb0e245cb3970478952e3d2da6
fabianagenila <fabianagenila@gmail.com>
2023-02-25 12:34:46 -0300

    about content and styles

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
 import styles from "./About.module.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        Sobre o mini <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consistem em um blog feito com React no frontend e Firebase
+        no backend.
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar post
+      </Link>
     </div>
   );
 };
