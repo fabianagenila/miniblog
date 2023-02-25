0b83c0916c528e9b060c78dade9e8122dfc1b6c2
fabianagenila <fabianagenila@gmail.com>
2023-02-25 13:32:58 -0300

    login user

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -67,6 +67,31 @@ export const useAuthentication = () => {
     signOut(auth);
   };
 
+  const login = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta!";
+      } else {
+        systemErrorMessage = "Ocorreu um erro! Por favor tente mais tarde";
+      }
+
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -77,5 +102,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login,
   };
 };
