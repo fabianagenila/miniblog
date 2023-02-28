171b21534ef077f7fbe0668e36d8f4a22b9cbbe2
fabianagenila <fabianagenila@gmail.com>
2023-02-28 17:30:47 -0300

    loading ternary at Post Page

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -4,10 +4,11 @@ import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 const Post = () => {
   const { id } = useParams();
-  const { document: post } = useFetchDocument("posts", id);
+  const { document: post, loading } = useFetchDocument("posts", id);
 
   return (
     <div>
+      {loading && <p>Carregando post...</p>}
       {post && (
         <>
           <h1>{post.title}</h1>
