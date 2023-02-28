3f75251c80d838105e992f1591be74d48f92168c
fabianagenila <fabianagenila@gmail.com>
2023-02-28 18:27:34 -0300

    Post page CSS

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -5,13 +5,26 @@ import { useFetchDocument } from "../../hooks/useFetchDocument";
 const Post = () => {
   const { id } = useParams();
   const { document: post, loading } = useFetchDocument("posts", id);
+  console.log("POST", post);
 
   return (
-    <div>
+    <div className={styles.post_container}>
       {loading && <p>Carregando post...</p>}
       {post && (
         <>
           <h1>{post.title}</h1>
+          <img src={post.image} alt={post.image} />
+          <p>{post.body}</p>
+          <h3>Assuntos deste post:</h3>
+          <div className={styles.tags}>
+            {post &&
+              post.tagsArray.map((tag) => (
+                <p key={tag}>
+                  <span>#</span>
+                  {tag}
+                </p>
+              ))}
+          </div>
         </>
       )}
     </div>
