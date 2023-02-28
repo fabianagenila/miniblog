6ea37100059c8cd67ea373820703e9529d4e57ca
fabianagenila <fabianagenila@gmail.com>
2023-02-28 17:28:38 -0300

    show post  by id data and hook

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,12 +1,18 @@
 import { useParams } from "react-router-dom";
 import styles from "./Post.module.css";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 const Post = () => {
   const { id } = useParams();
+  const { document: post } = useFetchDocument("posts", id);
 
   return (
     <div>
-      <h1>Post: {id}</h1>
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+        </>
+      )}
     </div>
   );
 };
