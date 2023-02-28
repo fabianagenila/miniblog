bf33a5fc81222d1e5057ec41842ca751e02924bb
fabianagenila <fabianagenila@gmail.com>
2023-02-28 16:47:25 -0300

    shows search results at Search Page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,14 +1,29 @@
 import styles from "./Search.module.css";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
+import PostDetail from "../../components/PostDetail";
+import { Link } from "react-router-dom";
 
 const Search = () => {
   const query = useQuery();
   const search = query.get("q");
+
+  const { documents: posts } = useFetchDocuments("posts", search);
+
   return (
-    <div>
-      <h2>Search</h2>
-      <p>{search}</p>
+    <div className={styles.search_container}>
+      <h2>Resultado da busca:</h2>
+      <div>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts resultantes da sua busca :(</p>
+            <Link to="/" className="btn btn-dark">
+              Voltar
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 };
